d99ab85c76b921ce81205d738c551cea39965440
krelunaid <krelunaid@gmail.com>
2026-08-27 18:50:03 +0200

    Clear Risonix test commerce data

---
+++ drizzle/0003_clear_risonix_test_data.sql
@@ -0,0 +1,9 @@
+DELETE FROM `risonix_order_events`;
+--> statement-breakpoint
+DELETE FROM `risonix_activations`;
+--> statement-breakpoint
+DELETE FROM `risonix_license_events`;
+--> statement-breakpoint
+DELETE FROM `risonix_licenses`;
+--> statement-breakpoint
+DELETE FROM `risonix_orders`;
